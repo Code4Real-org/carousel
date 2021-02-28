4c70adfb809b72527eb067807e3ae1d453070396
alizhang21 <75001461+alizhang21@users.noreply.github.com>
2021-02-28 11:43:24 -0800

    Update TeacherDashboard.vue

--- frontend/src/components/TeacherDashboard.vue
+++ frontend/src/components/TeacherDashboard.vue
@@ -69,20 +69,17 @@ export default {
 </script>
 
 <style scoped>
-
 /* background color */
 body {
   background-color: #f5f7e9ff;
   font-family: 'Varela Round', sans-serif;
 }
-
 /* Navbar container */
 .navigator {
   overflow: hidden;
   background-color: #779fa1ff;
   font-family: Varela Round, sans-serif;;
 }
-
 /* Links inside the navbar left */
 .navigator .left-side a {
   float: left;
@@ -92,12 +89,10 @@ body {
   padding: 14px 16px;
   text-decoration: none;
 }
-
 /*Right side of nav bar */
 .navigator .right-side {
   float: right;
 }
-
 /* Links inside navbar right */
 .navigator .right-side a {
   float: left;
@@ -107,13 +102,12 @@ body {
   padding: 14px 16px;
   text-decoration: none;
 }
-
 /* The dropdown container */
 .dropdown {
   float: left;
-  overflow: auto;
+  overflow-y: auto;
+  overflow-x: hidden;
 }
-
 /* Dropdown button */
 .dropdown .drop-button {
   font-size: 16px;
@@ -127,18 +121,15 @@ body {
   margin: 0;
   /* Important for vertical align on mobile phones */
 }
-
 /* Add a purple background color to navbar links on hover */
 .navigator .right-side a:hover,
 .dropdown:hover .drop-button {
   background-color: #564154ff;
 }
-
 /*Add a purple background color to nav bar links on hover (left) */
 .navigator .left-side a:hover {
   background-color: #564154ff;
 }
-
 /* Dropdown content (hidden by default) */
 .button {
   display: none;
@@ -148,7 +139,6 @@ body {
   box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
   z-index: 1;
 }
-
 /* Links inside the dropdown */
 .button a {
   float: none;
@@ -158,12 +148,10 @@ body {
   display: block;
   text-align: left;
 }
-
 /* Show the dropdown menu on hover */
 .dropdown:hover .button {
   display: block;
 }
-
 /* box for form */
 #form {
   width: 850px;
@@ -175,7 +163,6 @@ body {
   margin-top: 1%;
   margin-bottom: 1%;
 }
-
 /* box for title */
 #title {
   width: 600px;
@@ -185,7 +172,6 @@ body {
   margin-right: auto;
   margin-top: 2%;
 }
-
 /* box for description */
 #description {
   width: 600px;
@@ -195,7 +181,6 @@ body {
   margin-right: auto;
   margin-top: 10%;
 }
-
 /* box for x-button */
 .x-button {
   width: 20px;
@@ -203,7 +188,6 @@ body {
   background-color: #c95237ff;
   float: right;
 }
-
 /* letter in the x-button */
 .x-button a {
   float: none;
@@ -239,7 +223,6 @@ body {
   margin-right: auto;
   margin-top: 5%
 }
-
 .header {
   background-color: #deb887ff;
   margin: 1% 7% 1% 7%;
@@ -249,16 +232,13 @@ body {
   border-style: solid;
   border-width: 1px;
 }
-
 .quick-access-header {
   margin: 1% 10% 1% 10%;
 }
-
 .quick-access {
   margin: 1% 10% 1% 10%;
   text-align: center;
 }
-
 .recent-forms {
   padding: 5px;
   border-color: black;
@@ -273,12 +253,10 @@ body {
 .folders-header {
   margin: 1% 10% 1% 10%;
 }
-
 .folders {
   margin: 1% 10% 1% 10%;
   text-align: center;
 }
-
 .folder-item {
   border-color: black;
   border-style: solid;
@@ -289,25 +267,20 @@ body {
   display: inline-block;
   margin: 1%;
   border-radius: 15px;
-
 }
-
 .forms-header {
   margin: 1% 10% 1% 10%;
 }
-
 .forms{
   margin: 1% 10% 1% 10%;
   text-align: center;
 }
-
 .form-item{
   background-color: #e1caabff;
   width: 100%;
   padding-top: 100px;
   padding-bottom: 100px;
 }
-
 .tip-dropdown {
   float: right;
   text-align: center;
@@ -321,28 +294,24 @@ body {
   text-decoration: none;
   font-size: 28pt;
 }
-
 .tip-dropdown-content {
   display: none;
   position: absolute;
   background-color: #f9f9f9;
-  min-width: 30px;
   font-size: 10pt;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  padding: 12px 16px;
-  z-index: 1;
   color: white;
+  text-decoration: none;
+  margin-top: 3%;
+  z-index: 1;
 }
-
 .tip-dropdown:hover .tip-dropdown-content {
   display: inline;
-  background-color: gray;
-  color: white;
-
+  background-color: grey;
+  opacity: 90%;
+  color: black;
 }
 a:link {
   text-decoration: none;
   color: white;
 }
-
 </style>
