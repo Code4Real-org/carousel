2a9f4ef49f517f4c84fbff4abfd6d6b1efdc85e2
Florence Zhao <florence.y.zhao@gmail.com>
2021-08-20 21:58:22 -0700

    #10: remove unused files

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,143 +1,161 @@
 <template>
-  <div id="dashboard">
-    <transition name="fade">
-      <CommentModal v-if="showCommentModal" :post="selectedPost" @close="toggleCommentModal()"></CommentModal>
-    </transition>
-    <section>
-      <div class="col1">
-        <div class="profile">
-          <h5>{{ userProfile.name }}</h5>
-          <p>{{ userProfile.title }}</p>
-          <div class="create-post">
-            <p>create a post</p>
-            <form @submit.prevent>
-              <textarea v-model.trim="post.content"></textarea>
-              <button @click="createPost()" :disabled="post.content === ''" class="button">post</button>
-            </form>
-          </div>
-        </div>
-      </div>
-      <div class="col2">
-        <div v-if="posts.length">
-          <div v-for="post in posts" :key="post.id" class="post">
-            <h5>{{ post.userName }}</h5>
-            <span>{{ post.createdOn | formatDate }}</span>
-            <p>{{ post.content | trimLength }}</p>
-            <ul>
-              <li><a @click="toggleCommentModal(post)">comments {{ post.comments }}</a></li>
-              <li><a @click="likePost(post.id, post.likes)">likes {{ post.likes }}</a></li>
-              <li><a @click="viewPost(post)">view full post</a></li>
-            </ul>
-          </div>
-        </div>
-        <div v-else>
-          <p class="no-results">There are currently no posts</p>
-        </div>
-      </div>
-    </section>
-
-    <!-- full post modal -->
-    <transition name="fade">
-      <div v-if="showPostModal" class="p-modal">
-        <div class="p-container">
-          <a @click="closePostModal()" class="close">close</a>
-          <div class="post">
-            <h5>{{ fullPost.userName }}</h5>
-            <span>{{ fullPost.createdOn | formatDate }}</span>
-            <p>{{ fullPost.content }}</p>
-            <ul>
-              <li><a>comments {{ fullPost.comments }}</a></li>
-              <li><a>likes {{ fullPost.likes }}</a></li>
-            </ul>
-          </div>
-          <div v-show="postComments.length" class="comments">
-            <div v-for="comment in postComments" :key="comment.id" class="comment">
-              <p>{{ comment.userName }}</p>
-              <span>{{ comment.createdOn | formatDate }}</span>
-              <p>{{ comment.content }}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </transition>
+  <div>
+    <!-- <button v-google-signin-button="clientId" class="google-signin-button" data-onsuccess="onSignIn" data-scope="https://www.googleapis.com/auth/userinfo.profile">Register With Google</button> -->
+  <button v-if="!authenticated" @click="login">Login</button>
+  <div v-if="authenticated">
+  <button @click="logout">Logout</button>
+  <h1>Hi {{ firstName }}!</h1>
+  </div>
+  <div class="teacher">Teacher (Temp Pic)
+    <div class="teacher_picture">
+      <img src="https://i.pinimg.com/564x/9e/6f/f7/9e6ff7da87f11056c7cff3e2f525b9b9.jpg"/>
+    </div>
+  </div>
+  <div class="student">Student (Temp Pic)
+    <div class="student_picture">
+        <img src="https://i.pinimg.com/originals/9b/33/42/9b3342ee4de416e533a5de91d388feb3.jpg"/>
+    </div>
+  </div>
   </div>
 </template>
 
+
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+
 <script>
-import { commentsCollection } from '@/firebase'
-import { mapState } from 'vuex'
-import moment from 'moment'
-import CommentModal from '@/components/CommentModal'
-
+</script>
+<script>
+import Firebase from '../firebase.js';
 export default {
-  components: {
-    CommentModal
+  name: 'HelloWorld',
+  props: {
+    msg: String
   },
-  data() {
-    return {
-      post: {
-        content: ''
-      },
-      showCommentModal: false,
-      selectedPost: {},
-      showPostModal: false,
-      fullPost: {},
-      postComments: []
-    }
-  },
-  computed: {
-    ...mapState(['userProfile', 'posts'])
-  },
-  methods: {
-    createPost() {
-      this.$store.dispatch('createPost', { content: this.post.content })
-      this.post.content = ''
-    },
-    toggleCommentModal(post) {
-      this.showCommentModal = !this.showCommentModal
-
-      // if opening modal set selectedPost, else clear
-      if (this.showCommentModal) {
-        this.selectedPost = post
-      } else {
-        this.selectedPost = {}
+  data () {
+      return {
+        user: {
+          loggedIn: false,
+          data: {}
+        }
       }
     },
-    likePost(id, likesCount) {
-      this.$store.dispatch('likePost', { id, likesCount })
+    computed: {
+        authenticated(){
+          return this.user.loggedIn
+        },
+        firstName(){
+          if (this.user.data.displayName) {
+            return this.user.data.displayName.split(' ')[0]
+          }
+          return null
+        }
     },
-    async viewPost(post) {
-      const docs = await commentsCollection.where('postId', '==', post.id).get()
-
-      docs.forEach(doc => {
-        let comment = doc.data()
-        comment.id = doc.id
-        this.postComments.push(comment)
+    methods: {
+      login() {
+        Firebase.login();
+      },
+      logout() {
+        Firebase.logout()
+      }
+    },
+    mounted() {
+      Firebase.auth.onAuthStateChanged( user => {
+        if (user) {
+          this.user.loggedIn = true;
+          this.user.data = user;
+        }
+        else {
+          this.user.loggedIn = false;
+          this.user.data = {};
+        }
       })
-
-      this.fullPost = post
-      this.showPostModal = true
-    },
-    closePostModal() {
-      this.postComments = []
-      this.showPostModal = false
     }
-  },
-  filters: {
-    formatDate(val) {
-      if (!val) { return '-' }
-
-      let date = val.toDate()
-      return moment(date).fromNow()
-    },
-    trimLength(val) {
-      if (val.length < 200) { return val }
-      return `${val.substring(0, 200)}...`
-    }
-  }
 }
 </script>
 
-<style lang="scss" scoped>
+<style>
+.google-signin-button {
+  color: white;
+  background-color: red;
+  height: 50px;
+  font-size: 16px;
+  border-radius: 10px;
+  padding: 10px 20px 25px 20px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
 
+.teacher {
+  width: 47%;
+  height: 700px;
+  background-color: #ccccccff;
+  text-align: center;
+  float: left;
+  margin-left: 1%;
+  margin-top: 2%;
+  border: 1px solid black;
+}
+
+.student {
+  width: 47%;
+  height: 700px;
+  background-color: #ccccccff;
+  text-align: center;
+  float: right;
+  margin-right: 1%;
+  margin-top: 2%;
+  border: 1px solid black;
+}
+
+.teacher_picture {
+  position: relative;
+  width: 400px;
+  height: 400px;
+  border-radius: 50%;
+  background-color: #eeeeeeff;
+  margin: auto;
+  margin-top: 25%;
+  overflow: hidden;
+  border: 1px solid black;
+}
+
+.teacher_picture img {
+  width: auto;
+  height: 100%;
+  margin-left: -85px;
+  -webkit-transition: all 1s ease;
+   -moz-transition: all 1s ease;
+   -o-transition: all 1s ease;
+   -ms-transition: all 1s ease;
+   transition: all 1s ease;
+}
+
+.teacher_picture img:hover {
+  -webkit-filter: brightness(70%);
+   filter: brightness(70%);
+}
+.student_picture {
+  width: 400px;
+  height: 400px;
+  border-radius: 50%;
+  background-color: #eeeeeeff;
+  margin: auto;
+  margin-top: 25%;
+  border: 1px solid black;
+}
+
+.student_picture img {
+  width: auto;
+  height: 100%;
+  border-radius: 50%;
+  -webkit-transition: all 1s ease;
+   -moz-transition: all 1s ease;
+   -o-transition: all 1s ease;
+   -ms-transition: all 1s ease;
+   transition: all 1s ease;
+}
+
+.student_picture img:hover {
+  -webkit-filter: brightness(70%);
+   filter: brightness(70%);
+}
 </style>
