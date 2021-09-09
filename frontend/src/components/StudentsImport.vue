79067231d88d88ed26753e9a14c4205515718bd5
Florence Zhao <florence.y.zhao@gmail.com>
2021-09-08 19:23:18 -0700

    #11: do student list importing in a popup window

---
+++ frontend/src/components/StudentsImport.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="c-modal">
+    <div class="c-container">
+      <a @click="$emit('close')">close</a>
+
+  <div>
+    <h5>Import a student roster</h5>
+    <upload-file :activeAssignment="activeAssignment"></upload-file>
+  </div>
+
+  </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import UploadFile from "../components/UploadFile"
+
+export default {
+  name: "students-import",
+  components: {
+    UploadFile
+  },
+  data() {
+    return {
+      students: []
+    };
+  },
+  computed: {
+    ...mapState(['activeAssignment'])
+  },
+  methods: {
+
+  }
+};
+</script>
