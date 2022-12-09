22d441d6ff5c7eaa8746cb5a637ec69da1bfc08e
Debian <bitnami@ip-172-26-13-79.us-west-2.compute.internal>
2022-12-09 07:41:34 +0000

    #38: fix the display of the Wikipedia page link

--- frontend/src/components/LotteryModal.vue
+++ frontend/src/components/LotteryModal.vue
@@ -18,7 +18,7 @@
               <input type="text" placeholder="Last name" id="lname" v-model.trim.lazy="entry.lastName" required maxlength="64" :disabled="isLocked">-->
               <label v-if="poasStats[index] && poasStats[index][0] == -1" id="counter" width:50px><b><i>taken</i></b></label>
               <label v-else id="counter" v-for="(count, preference) in poasStats[index]" :key="index + preference" width:150px> Opt. {{preference+1}}: {{count}}</label>
-              <p v-if="entry.wikiLink"> Wikipedia Page: <a :href = "entry.wikiLink" > {{entry.wikiLink}} </a></p>
+              <p v-if="entry.wikiLink"> Wikipedia Page: <a :href = "entry.wikiLink" target="_blank"> {{entry.wikiLink}} </a></p>
               <p v-if="entry.wikiDescription"> {{entry.wikiDescription}}... </p>
               <br><br>
               <h5>What is the title of the (auto)biography? (limit of 100 characters)</h5>
