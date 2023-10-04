7beb5109e2355637d120a620bc256be5b0941f2b
Florence Zhao <florence.y.zhao@gmail.com>
2023-10-03 18:28:53 -0700

    Revert "#39: fix bug where accessing carousel causes an access error to loop infinitely"
    This reverts commit 496ae65eaacbdfbc66bcc0a2a529fd25b2b927c7.
    
    revert commit

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -33,14 +33,9 @@ http.interceptors.request.use (
 http.interceptors.response.use((response) => {
   return response
 }, function (error) {
-  if (error.response && error.response.status === 401) {
-    router.push('/login');
-  } else {
-    VueSimpleAlert.alert("Please access the correct dashboard.");
-    router.push('/');
-  }
-  console.log(error);
-  
+  VueSimpleAlert.alert("Please access the correct dashboard.");
+
+  router.push('/');
   return Promise.reject(error);
 })
 
